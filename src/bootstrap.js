abe0acec6374409902bfba512645aca5f3f2b141
John Angel Henao <jhon.henao@omni.pro>
2024-05-04 14:09:47 -0500

    prueba origen

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,22 +1,17 @@
-import React, { useState } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 import Form from "./components/Form";
 
-const App = () => {
-  const [pais, setPais] = useState(null); // Estado para almacenar el valor de Pais
+import "./index.css";
 
-  // Función para recibir y mostrar la propiedad Pais
-  const handleSetPais = (paisValue) => {
-    console.log("Pais en App:", paisValue);
-    setPais(paisValue);
-  };
-
-  return (
-    <div className="container">
-      <Form setPais={handleSetPais} />
-    </div>
-  );
-};
-
+// Utiliza createRoot en lugar de render
 const root = createRoot(document.getElementById("app"));
+
+const App = () => (
+  <div className="container">
+    <Form />
+  </div>
+);
+
+// Renderiza dentro del root creado con createRoot
 root.render(<App />);
