6ea68149274ede53abb1ca9f43046d17704136a3
John Angel Henao <jhon.henao@omni.pro>
2024-05-31 12:11:41 -0500

    Se realiza creacion de segundo formulario PoC GFF AppShell tamaño contenedor

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Form from "./components/Form";
-
+import FormTwo from "./components/FormTwo";
 import "./index.css";
 
 // Utiliza createRoot en lugar de render
 const root = createRoot(document.getElementById("app"));
 
 const App = () => (
-  <div className="container">
-    <Form Pais="Colombia - App"/>
-  </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Form />} />
+      <Route path="/form-two" element={<FormTwo />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // Renderiza dentro del root creado con createRoot
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
