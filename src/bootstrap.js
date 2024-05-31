dab8f69988bc57c5dfd17ad4ac5fc7469fc074b1
John Angel Henao <jhon.henao@omni.pro>
2024-05-31 15:11:32 -0500

    Prueba por fda

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,17 +3,20 @@ import { createRoot } from "react-dom/client";
 import Form from "./components/Form";
 import FormTwo from "./components/FormTwo";
 import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Utiliza createRoot en lugar de render
 const root = createRoot(document.getElementById("app"));
 
 const App = () => (
-  <div>
-    <Form />
-    <FormTwo />
-  </div>
-
+  <Router>
+    <Routes>
+      <Route path="/" element={<Form />} />
+      <Route path="/form-two" element={<FormTwo />} />
+    </Routes>
+  </Router>
 );
 
-// Renderiza dentro del root creado con createRoot
+
+// Renderiza dentro del root creado con createRoot 
 root.render(<App />);
